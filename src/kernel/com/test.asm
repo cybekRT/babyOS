7e2cd369ab7624f1b581c4acc3e906f5ca949994
cybek <cybek@wyobraz.net>
2020-02-29 17:21:26 +0100

    Testing position independent code

--- src/kernel/com/test.asm
+++ src/kernel/com/test.asm
@@ -1,14 +1,29 @@
 [bits 32]
+main_std:
+	;mov	byte [col], 0
+	;mov	al, 0
 main:
+	;xchg	bx, bx
+	push	eax
+	mov	al, [eax + col]
 	sti
-	mov	edi, 0xa0000
-	mov	al, [col]
+	std
+	mov	edi, 0xa0000 + 320*100
+	;lea	esi, [rel col]
+	;mov	al, [esi]
+	;mov	al, [rel col]
 	mov	ecx, 320*100
 	rep	stosb
-	inc	byte [col]
+	cld
+	;std
+	;inc	byte [col]
+	;inc	al
+	;cld
 
 	mov	ecx, 0xffffff;ff
 	loop	$
+	pop	eax
+	inc	byte [eax + col]
 	jmp	main
 
 	ret
